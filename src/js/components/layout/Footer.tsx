2a893434b2bb51fe1d31648c013dc6220c5c050e
koszetrawnik <koszetrawnik@gmail.com>
2026-03-15 14:38:13 +0100

    add droids to footer

--- src/js/components/layout/Footer.tsx
+++ src/js/components/layout/Footer.tsx
@@ -5,7 +5,7 @@ export const Footer = () => {
     <footer className="fixed bottom-0 left-0 right-0 bg-background">
       <Separator />
       <p className="text-center text-[10px] text-gray-300 py-1">
-        created with ❤️ by bukala
+        created with ❤️ 🤖 by bukala
       </p>
     </footer>
   );
